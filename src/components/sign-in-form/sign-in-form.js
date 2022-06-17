0325594989549c44353e202a361a27dd884e4f9f
jayhan1109 <jayhan1109@gmail.com>
2022-06-17 10:36:42 -0700

    Fix CSS issue

--- src/components/sign-in-form/sign-in-form.js
+++ src/components/sign-in-form/sign-in-form.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import {
-  createUserDocumentFromAuth,
   signInAuthUserWithEmailAndPassword,
   signInWithGooglePopup,
 } from "../../utils/firebase/firebase";
